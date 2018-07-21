b4e33edfe7ab6e35a92d038e15f963df2d969290
Osvaldo Fernandez <osvaldofernandez@KAIS-MAC43.fritz.box>
2018-07-21 15:18:48 +0200

    added remove person dialog

---
+++ src/app/removePerson/removePerson.component.ts
@@ -0,0 +1,25 @@
+import {Component} from "@angular/core";
+import {Person} from "../model/Person";
+import {AppState} from "../model/appState";
+import {Store} from "@ngrx/store";
+
+@Component({
+  selector: 'addPerson',
+  templateUrl: './addPerson.component.html'
+})
+
+
+export class AddPersonComponent {
+
+  public person: Person;
+
+  public constructor(private store: Store<AppState>) {
+    this.person = new Person();
+  }
+
+  addPerson() {
+    console.log(this.person.name);
+    this.store.dispatch({type: "ADD_PERSON", payload: this.person});
+    this.person = new Person();
+  }
+}
