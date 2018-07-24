5becdf205b4dc0ffab18e10ed54ece112ce93588
Osvaldo Fernandez <osvaldofernandez@KAIS-MAC43.fritz.box>
2018-07-24 16:59:38 +0200

    fixed pipe for lower case

--- src/app/pipe/personPipe.ts
+++ src/app/pipe/personPipe.ts
@@ -6,6 +6,6 @@ export class PersonPipe implements PipeTransform {
   transform(persons: Person[], value: string) {
     if (!value) return persons;
     value = value.toLowerCase();
-    return persons.filter(person => person.name.indexOf(value) !== -1 || person.amountOfDebt.toString().indexOf(value) !== -1);
+    return persons.filter(person => person.name.toLowerCase().indexOf(value) !== -1 || person.amountOfDebt.toString().indexOf(value) !== -1);
   }
 }
